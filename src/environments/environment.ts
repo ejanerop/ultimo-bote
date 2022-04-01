b1d0a1fddf3e494b2c4def098353b1c08bc2cd94
Eric Janero <ejanerop97@gmail.com>
2022-04-01 14:06:38 -0400

    remoed keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'ultimo-bote-app',
-    appId: '1:832357320209:web:c55d21a6ab9fc82abd53b0',
-    storageBucket: 'ultimo-bote-app.appspot.com',
-    apiKey: 'AIzaSyDC7D-zKMxXXu7VIcn_9vK3skE3nvmekDE',
-    authDomain: 'ultimo-bote-app.firebaseapp.com',
-    messagingSenderId: '832357320209',
-    measurementId: 'G-86PJZ8189S',
-  },
-  production: false
+  production: false,
 };
 
 /*
