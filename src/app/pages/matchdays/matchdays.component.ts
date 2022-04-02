58e2256bebc5f5f024978bdda26a6dbb1de3f833
Eric Janero <ejanerop97@gmail.com>
2022-04-02 16:09:56 -0400

    List of matchdays

--- src/app/pages/matchdays/matchdays.component.ts
+++ src/app/pages/matchdays/matchdays.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { map } from 'rxjs';
+import { ConfirmDialogComponent } from 'src/app/components/confirm-dialog/confirm-dialog.component';
 import { MatchdayDialogComponent } from 'src/app/components/matchday-dialog/matchday-dialog.component';
 import { MatchdayService } from 'src/app/services/matchday.service';
 import Swal from 'sweetalert2';
@@ -18,12 +21,34 @@ const Toast = Swal.mixin({
   styleUrls: ['./matchdays.component.scss'],
 })
 export class MatchdaysComponent implements OnInit {
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+  players: any;
+  displayedColumns: string[] = ['date', 'assist', 'actions'];
   constructor(
     private matchdayService: MatchdayService,
     public dialog: MatDialog
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.matchdayService
+      .getMatchdays()
+      .pipe(
+        map((items: any) => {
+          return items.map((item: any) => {
+            return {
+              id: item.id,
+              dateTime: new Date(item.data().date.seconds * 1000),
+              ...item.data(),
+            };
+          });
+        })
+      )
+      .subscribe((data: any) => {
+        console.log(data);
+
+        this.dataSource = new MatTableDataSource(data);
+      });
+  }
 
   create() {
     const dialogRef = this.dialog.open(MatchdayDialogComponent, {
@@ -49,6 +74,28 @@ export class MatchdaysComponent implements OnInit {
             title: 'There was an error',
           });
         });
+    });
+  }
+
+  remove(element: any) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.matchdayService
+          .removeMatchday(element)
+          .then((res) => {
+            Toast.fire({
+              icon: 'success',
+              title: 'Removed successfully',
+            });
+          })
+          .catch((err) => {
+            Toast.fire({
+              icon: 'error',
+              title: 'There was an error',
+            });
+          });
+      }
     });
   }
 }
